54bba20ea038bf30119f4a24bcc5be8d8e661096
nadia <anabelle0025@yahoo.fr>
2023-10-27 21:14:31 +0200

    finir les css

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,31 +36,62 @@ pokemonList.appendChild(option);
 // ..............faire un edventlisstener pour chaque chois il ya un chang et option....
    
 
-    if (option.innerText === "--Choix Pokemon--") {
-        // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
-        image.setAttribute("src", ''); // Effacer l'image
-      } else {
-        // Le code pour gérer la sélection d'un Pokémon
-        pokemonList.addEventListener("change", () => {
-          console.log("Pokemon sélectionné :", pokemonList.value);
-          let pokemonTrouve = datasAxios.find((element) => element.name === pokemonList.value);
+    // if (option.innerText === "--Choix Pokemon--") {
+    //     // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
+    //     image.setAttribute("src", ''); // Effacer l'image
+    //   } else {
+    //     // Le code pour gérer la sélection d'un Pokémon
+    //     pokemonList.addEventListener("change", () => {
+    //       console.log("Pokemon sélectionné :", pokemonList.value);
+    //       let pokemonTrouve = datasAxios.find((element) => element.name === pokemonList.value);
           
-          if (pokemonTrouve) {
-            console.log("Image du Pokémon :", pokemonTrouve.image);
-            image.setAttribute("src", pokemonTrouve.image);
-          } else {
-            console.log("Aucun Pokémon trouvé pour le nom sélectionné :", pokemonList.value);
-            // Effacer l'image
-            image.setAttribute("src", '');
-          }
-        });
-      }
-      //........cherches les elements(proprietés) de chaque pokemon........
+    //       if (pokemonTrouve) {
+    //         console.log("Image du Pokémon :", pokemonTrouve.image);
+    //         image.setAttribute("src", pokemonTrouve.image);
       
-      for (const [propriete, valeur] of Object.entries(pokemonTrouve.stats)) {  //METHODE: object.entries()
-        console.log(`${propriete}: ${valeur}`);
-        let uneStat = document.createElement("div");
-        uneStat.classList.add("une-statistique");
-        uneStat.textContent = propriete + " : " +  valeur;
-        main.appendChild(uneStat);
+    //       } else {
+    //         console.log("Aucun Pokémon trouvé pour le nom sélectionné :", pokemonList.value);
+    //         // Effacer l'image
+    //         image.setAttribute("src", '');
+    //       }
+    //     });
+    //   }
+      //........cherches les elements(proprietés) de chaque pokemon........
+
+    if (option.innerText === "--Choix Pokemon--") {
+      // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
+      image.setAttribute("src", ''); // Effacer l'image
+      main.innerHTML = ''; // Effacer les statistiques
+    } else {
+      // Le code pour gérer la sélection d'un Pokémon
+      pokemonList.addEventListener("change", () => {
+        const selectedPokemonName = pokemonList.value;
+        console.log("Pokemon sélectionné :", selectedPokemonName);
+    
+        // Trouver le Pokémon sélectionné
+        const selectedPokemon = datasAxios.find((element) => element.name === selectedPokemonName);
+    
+        if (selectedPokemon) {
+          console.log("Image du Pokémon :", selectedPokemon.image);
+          image.setAttribute("src", selectedPokemon.image);
+    
+          // Effacer les statistiques précédentes
+          // main.innerHTML = '';
+    
+          // Afficher les statistiques
+          for (const [propriete, valeur] of Object.entries(selectedPokemon.stats)) {
+            console.log(`${propriete}: ${valeur}`);
+            const uneStat = document.createElement("table");
+            uneStat.classList.add("une-statistique");
+            uneStat.textContent = propriete + " : " + valeur;
+            main.appendChild(uneStat);
+          }
+        } else {
+          console.log("Aucun Pokémon trouvé pour le nom sélectionné :", selectedPokemonName);
+          // Effacer l'image
+          image.setAttribute("src", '');
+          // Effacer les statistiques
+          main.innerHTML = '';
+        }
+      });
     }
