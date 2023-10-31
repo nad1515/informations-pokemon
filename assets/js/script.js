62a318b675cc1713259943f2ba63c4e85f574765
nadia <anabelle0025@yahoo.fr>
2023-10-31 12:30:58 +0100

    a terminer

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,9 @@ let image = document.createElement('img');
  image.setAttribute('id','pokemon-img');
  main.appendChild(image);
 
+ let contenair = document.createElement("div");
+ contenair.classList.add("contenair");
+ main.appendChild(contenair);
 
 //...............afficher la list des pokemons avec une boucle foreach...........
 let option = document.createElement("option");
@@ -36,62 +39,42 @@ pokemonList.appendChild(option);
 // ..............faire un edventlisstener pour chaque chois il ya un chang et option....
    
 
-    // if (option.innerText === "--Choix Pokemon--") {
-    //     // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
-    //     image.setAttribute("src", ''); // Effacer l'image
-    //   } else {
-    //     // Le code pour gérer la sélection d'un Pokémon
-    //     pokemonList.addEventListener("change", () => {
-    //       console.log("Pokemon sélectionné :", pokemonList.value);
-    //       let pokemonTrouve = datasAxios.find((element) => element.name === pokemonList.value);
-          
-    //       if (pokemonTrouve) {
-    //         console.log("Image du Pokémon :", pokemonTrouve.image);
-    //         image.setAttribute("src", pokemonTrouve.image);
-      
-    //       } else {
-    //         console.log("Aucun Pokémon trouvé pour le nom sélectionné :", pokemonList.value);
-    //         // Effacer l'image
-    //         image.setAttribute("src", '');
-    //       }
-    //     });
-    //   }
       //........cherches les elements(proprietés) de chaque pokemon........
 
     if (option.innerText === "--Choix Pokemon--") {
       // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
       image.setAttribute("src", ''); // Effacer l'image
-      main.innerHTML = ''; // Effacer les statistiques
+    
     } else {
       // Le code pour gérer la sélection d'un Pokémon
       pokemonList.addEventListener("change", () => {
-        const selectedPokemonName = pokemonList.value;
-        console.log("Pokemon sélectionné :", selectedPokemonName);
-    
+        let PokemonName = pokemonList.value;
+        console.log("Pokemon sélectionné :", PokemonName);
+         
         // Trouver le Pokémon sélectionné
-        const selectedPokemon = datasAxios.find((element) => element.name === selectedPokemonName);
+        let selectePokemon = datasAxios.find((element) => element.name === PokemonName);
     
-        if (selectedPokemon) {
-          console.log("Image du Pokémon :", selectedPokemon.image);
-          image.setAttribute("src", selectedPokemon.image);
-    
-          // Effacer les statistiques précédentes
-          // main.innerHTML = '';
-    
+           console.log("Image du Pokémon :", selectePokemon.image);
+           image.setAttribute("src", selectePokemon.image);
+
+            
+
+           // Effacez les éléments enfants actuels de l'élément "contenair"
+           while (contenair.firstChild) {
+               contenair.removeChild(contenair.firstChild);
+               
+           }
+           
           // Afficher les statistiques
-          for (const [propriete, valeur] of Object.entries(selectedPokemon.stats)) {
+          for (let [propriete, valeur] of Object.entries(selectePokemon.stats)) {
             console.log(`${propriete}: ${valeur}`);
-            const uneStat = document.createElement("table");
+            let uneStat = document.createElement("table");
             uneStat.classList.add("une-statistique");
             uneStat.textContent = propriete + " : " + valeur;
-            main.appendChild(uneStat);
+            contenair.appendChild(uneStat);
           }
-        } else {
-          console.log("Aucun Pokémon trouvé pour le nom sélectionné :", selectedPokemonName);
-          // Effacer l'image
-          image.setAttribute("src", '');
-          // Effacer les statistiques
-          main.innerHTML = '';
-        }
+          
+        
       });
     }
+    
