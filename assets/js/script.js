703517fb9b8554cebd3387061213a5242fb70b3b
nadia <anabelle0025@yahoo.fr>
2023-10-31 15:46:55 +0100

    pokemon termine

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,71 +10,66 @@ async function axiosTest () {
 
 let main = document.createElement('main');
 document.body.appendChild(main);
-
+// Créez une liste select pour contenir les éléments pokemons
 let pokemonList = document.createElement('select'); 
-pokemonList.setAttribute('id', 'pokemonList');            // Créez une liste select pour contenir les éléments pokemons
+pokemonList.setAttribute('id', 'pokemonList');           
 main.appendChild(pokemonList);
 
-let image = document.createElement('img');
- image.setAttribute('id','pokemon-img');
- main.appendChild(image);
+let contenair = document.createElement("div");
+contenair.classList.add("contenair");
+main.appendChild(contenair);
 
- let contenair = document.createElement("div");
- contenair.classList.add("contenair");
- main.appendChild(contenair);
+let lesStats = document.createElement("table");
+contenair.appendChild(lesStats);
+
+let image = document.createElement('img');
+image.setAttribute('id','pokemon-img');
+contenair.appendChild(image);
+
 
 //...............afficher la list des pokemons avec une boucle foreach...........
-let option = document.createElement("option");
-option.innerText = "--Choix Pokemon--";//....choisir de rien afficher au depart et cest au client de choisir son pokemon.....
+let optionDefault = document.createElement("option");
+optionDefault.innerText = "--Choix Pokemon--";//....choisir de rien afficher au depart et cest au client de choisir son pokemon.....
+optionDefault.value = "0";
+pokemonList.appendChild(optionDefault);
+
+datasAxios.forEach(pokemon => {  // recuperer la liste des pokemons..............
+console.log("pokemon,", pokemon);
+let option = document.createElement('option');
+option.innerText = pokemon.name;
 pokemonList.appendChild(option);
+});
+        // ..............faire un edventlisstener pour chaque chois il ya un chang et option....
+       //........cherches les elements(proprietés) de chaque pokemon........
 
-    datasAxios.forEach(pokemon => {
-        console.log("pokemon,", pokemon);
-        option = document.createElement('option');
-        option.innerText = pokemon.name;
-        pokemonList.appendChild(option);
-    });
-
-
-// ..............faire un edventlisstener pour chaque chois il ya un chang et option....
-   
-
-      //........cherches les elements(proprietés) de chaque pokemon........
-
-    if (option.innerText === "--Choix Pokemon--") {
-      // Le code pour gérer le cas où "--Choix Pokemon--" est sélectionné
-      image.setAttribute("src", ''); // Effacer l'image
-    
-    } else {
       // Le code pour gérer la sélection d'un Pokémon
-      pokemonList.addEventListener("change", () => {
-        let PokemonName = pokemonList.value;
-        console.log("Pokemon sélectionné :", PokemonName);
+document.querySelector("select").addEventListener("change", () => {
+ document.querySelector("table").innerHTML = "";
+ let PokemonName = document.querySelector("select").value;
+ if (document.querySelector("select").value == "0") {
+ document.querySelector(".contenair img").removeAttribute("src");
+ } else {
          
         // Trouver le Pokémon sélectionné
-        let selectePokemon = datasAxios.find((element) => element.name === PokemonName);
-    
-           console.log("Image du Pokémon :", selectePokemon.image);
-           image.setAttribute("src", selectePokemon.image);
+ let selectePokemon = datasAxios.find((element) => element.name === PokemonName);
+ image.setAttribute("src", selectePokemon.image);
+       
 
-            
-
-           // Effacez les éléments enfants actuels de l'élément "contenair"
-           while (contenair.firstChild) {
-               contenair.removeChild(contenair.firstChild);
-               
-           }
-           
           // Afficher les statistiques
-          for (let [propriete, valeur] of Object.entries(selectePokemon.stats)) {
-            console.log(`${propriete}: ${valeur}`);
-            let uneStat = document.createElement("table");
-            uneStat.classList.add("une-statistique");
-            uneStat.textContent = propriete + " : " + valeur;
-            contenair.appendChild(uneStat);
-          }
+
+ for (let [propriete, valeur] of Object.entries(selectePokemon.stats)) {
+ console.log(`${propriete}: ${valeur}`);
+ let uneLigne = document.createElement("tr");
+ lesStats.appendChild(uneLigne);
+ let uneStat = document.createElement("td");
+ uneStat.classList.add("une-statistique");
+ uneStat.textContent = propriete + " : " + valeur;
+ uneLigne.appendChild(uneStat);
+  }
+ }
+        
           
         
-      });
-    }
+});
+
     
